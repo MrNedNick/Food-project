a3f5ba5b8f9f01c03dcb8417a83e469dd8a994c8
Nikita <mrnednick@gmail.com>
2021-10-20 14:04:13 +0300

    Lesson 53: Woorking with OpenServer

--- js/script.js
+++ js/script.js
@@ -215,5 +215,47 @@ window.addEventListener('DOMContentLoaded', () => {
         'menu__item'
     ).render();
 
+    // forms
+
+    const forms = document.querySelectorAll('form');
+
+    const message = {
+        loading: 'Загрузка',
+        succes: 'Спасибо! Скоро мы с вами свяжемся',
+        failure: 'Что-то пошло не так...'
+    };
+
+    forms.forEach(item => {
+        postData(item);
+    })
+
+    function postData(form) {
+        form.addEventListener('submit', () => {
+            e.preventDefault();
+
+            const statusMessage = document.createElement('div')
+            statusMessage.classList.add('status')
+            statusMessage.textContent = message.loading;
+            form.append(statusMessage);
+
+            const request = new XMLHttpRequest();
+            request.open('POST', 'server.php');
+
+            request.setRequestHeader('Content-type', 'multipart/form-data')
+            const formData = new FormData(form);
+
+            request.send(formData);
+
+            request.addEventListener('load', () => {
+                if (request.status === 200) {
+                    console.log(request.response)
+                    statusMessage.textContent = message.succes;
+                } else {
+                    statusMessage.textContent = message.failure
+                }
+            })
+        })
+    }
+
 
 });
